cfbc62cc45b5991e9f1693f9647a491153d88806
Arpan Silwal <102730491+Arpanc0des@users.noreply.github.com>
2024-03-21 14:35:25 -0400

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using YourProjectName.Models;
+
+namespace YourProjectName.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(string operation, double firstNumber, double secondNumber)
+        {
+            double result = 0;
+
+            switch (operation)
+            {
+                case "add":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "subtract":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "multiply":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "divide":
+                    if (secondNumber != 0)
+                    {
+                        result = firstNumber / secondNumber;
+                    }
+                    else
+                    {
+                        ViewBag.Result = "Cannot divide by zero";
+                        return View("Index");
+                    }
+                    break;
+                default:
+                    ViewBag.Result = "Invalid operation";
+                    return View("Index");
+            }
+
+            ViewBag.Result = "Result: " + result;
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
